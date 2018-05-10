d9c8535b76cfb5806c67d654f0e8777b3eb04ac7
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-10 15:07:38 +0400

    For importing modules, imports: {...} not needed; exports: {...} seems to be enough.

--- src/app/material-module.ts
+++ src/app/material-module.ts
@@ -3,19 +3,6 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
-    /* imports: [
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        MatGridListModule,
-        MatCardModule,
-        MatMenuModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule,
-    ], */
     exports: [
         MatToolbarModule,
         MatButtonModule,
