62f13b41d91d4c261dd07b2550769c487ec55945
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-11 10:13:17 +0400

    Enabling ng.profile (can be called from console); can check components on ng.profiler.appRef._componentFactoryResolver...

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ApplicationRef } from '@angular/core';
+import { enableDebugTools } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -9,4 +10,11 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .then(module => {
+    let
+      applicationRef = module.injector.get(ApplicationRef),
+      appComponent = applicationRef.components[0];
+
+    enableDebugTools(appComponent);
+  })
+  .catch(err => console.error(err));
